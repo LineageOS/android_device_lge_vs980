9735b5433087b3bc30215fb8c074afa808138e85
KhabarlakKonstantin <habarlack@gmail.com>
2017-09-03 11:14:19 +0300

    vs980: Rebrand common_full_phone.mk to vendor/lineage
    Change-Id: Id2c468fb7b115f82c2d8c6d278cbeb008e1efe09

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vs980/vs980.mk)
